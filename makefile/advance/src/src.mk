2853e55b2cc0f5ad04610df6f3ad343f98344044
gilgil <gilgil@gilgil.net>
2015-04-09 01:27:57 +0900

    Move default CPPFLAGS and LDLIBS option

--- makefile/advance/src/src.mk
+++ makefile/advance/src/src.mk
@@ -5,6 +5,4 @@
 SRC_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 SRC_DIR := $(subst $(_ABSDIR),$(_RELDIR),$(SRC_DIR))
 
-CPPFLAGS += -Wall -g
 CPPFLAGS += -I$(SRC_DIR)
-LDLIBS   += -lpthread
